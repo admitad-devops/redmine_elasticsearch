697e10172d2f85a5602e8ddb8efea1e210ddfc69
Boris Gorbylev <ekho@ekho.name>
2020-02-15 11:36:00 +0300

    Version bump

--- init.rb
+++ init.rb
@@ -4,7 +4,7 @@ Redmine::Plugin.register :redmine_elasticsearch do
   name        'Redmine Elasticsearch Plugin'
   description 'This plugin integrates the Elasticsearch full-text search engine into Redmine.'
   author      'Restream'
-  version     '0.2.1'
+  version     '0.2.2'
   url         'https://github.com/Restream/redmine_elasticsearch'
 
   requires_redmine version_or_higher: '2.1'
