28ca40f51810a92768fa65c7b03d5eeb70dcafcb
Boris Gorbylev <ekho@ekho.name>
2020-03-02 11:48:55 +0300

    Bump version

--- init.rb
+++ init.rb
@@ -4,7 +4,7 @@ Redmine::Plugin.register :redmine_elasticsearch do
   name        'Redmine Elasticsearch Plugin'
   description 'This plugin integrates the Elasticsearch full-text search engine into Redmine. Forked from https://github.com/Restream/redmine_elasticsearch'
   author      'Boris Gorbylev <b.gorbylev@admitad.com>'
-  version     '1.0.0'
+  version     '1.0.1'
   url         'https://github.com/admitad-devops/redmine_elasticsearch'
 
   requires_redmine version_or_higher: '2.1'
