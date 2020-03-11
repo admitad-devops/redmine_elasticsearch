3d01557a3ef616819e0143cf87816aee1f574437
Boris Gorbylev <ekho@ekho.name>
2020-03-11 10:26:09 +0300

    Author url

--- init.rb
+++ init.rb
@@ -3,7 +3,8 @@ require 'redmine'
 Redmine::Plugin.register :redmine_elasticsearch do
   name        'Redmine Elasticsearch Plugin'
   description 'This plugin integrates the Elasticsearch full-text search engine into Redmine. Forked from https://github.com/Restream/redmine_elasticsearch'
-  author      'Boris Gorbylev <b.gorbylev@admitad.com>'
+  author      'Boris Gorbylev'
+  author_url  'https://github.com/ekho'
   version     '1.0.1'
   url         'https://github.com/admitad-devops/redmine_elasticsearch'
 
